f4324c98e36bffe161b5dd34730ebf062225ca1a
zhihan <zhi.han@gmail.com>
2015-07-18 08:14:52 -0400

    Added commandline option and support jar files

--- src/main/groovy/Scanner.groovy
+++ src/main/groovy/Scanner.groovy
@@ -6,8 +6,16 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.Opcodes
 
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.CommandLineParser
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.GnuParser
+import org.apache.commons.cli.HelpFormatter
+
 import java.io.InputStream
 import java.io.FileInputStream
+import java.util.jar.JarFile
+import java.util.zip.ZipEntry
 
 import groovy.transform.CompileStatic
 
@@ -19,8 +27,7 @@ class AVisitor extends AnnotationVisitor {
 
     @Override
     public void visit(String name, Object value) {
-        println("Name is " + name)
-        println(":" + value)
+        println(name + ": " + value)
     }
 }
 
@@ -53,11 +60,23 @@ class CVisitor extends ClassVisitor {
 
 class Scanner {
     static void main(String[] args) {
-        String fileName = args[0]
+        Options options = new Options()
+        options.addOption("j", "jar", true, "Path to the jar file")
 
-        println(fileName)
+        CommandLineParser parser = new GnuParser();
+        CommandLine cmd = parser.parse(options, args);
+        String className = cmd.getArgs()[0]
+        String fileName = className.replace(".", "/") + ".class"
 
-        InputStream inS = new FileInputStream(fileName)
+        InputStream inS = null
+        if (cmd.hasOption("j")) {
+            JarFile jarFile = new JarFile(cmd.getOptionValue("j"))
+            ZipEntry entry = jarFile.getEntry(fileName)
+            inS = jarFile.getInputStream(entry)
+        } else {
+            inS = new FileInputStream(fileName)
+        }
+
         ClassReader reader = new ClassReader(inS)
         ClassVisitor visitor = new CVisitor()
         reader.accept(visitor, ClassReader.SKIP_FRAMES)
